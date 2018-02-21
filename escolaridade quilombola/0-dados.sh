32df878364c9d50c69d34d6f386ce04ae9f09032
Dandara Sousa <dandara.sousa@ccc.ufcg.edu.br>
2018-02-21 17:28:41 -0300

    organiza a forma de fazer o join

--- escolaridade quilombola/0-dados.sh
+++ escolaridade quilombola/0-dados.sh
@@ -1,10 +1,10 @@
 
 dsv2json  -r ',' -n < ../dados/estabelecimentos-ef-quilombo.csv > dado1-ensino-quilombo.ndjson
-ndjson-split 'd.features' < ../dados/geo1-br_municipios_projetado.json | ndjson-map 'd.localidade = d.properties.NOME, d' > geo2-br_municipios.ndjson
-ndjson-map 'd.localidade = d.Localidade.toUpperCase(), d' < dado1-ensino-quilombo.ndjson > dado2-ensino-quilombo-comchave.ndjson
+ndjson-split 'd.features' < ../dados/geo1-br_municipios_projetado.json | ndjson-map 'd.Localidade = d.properties.NOME, d' > geo2-br_municipios.ndjson
+ndjson-map 'd.Localidade = d.Localidade.toUpperCase(), d' < dado1-ensino-quilombo.ndjson > dado2-ensino-quilombo-comchave.ndjson
 
 EXP_PROPRIEDADE='d[0].properties = Object.assign({}, d[0].properties, d[1]), d[0]'
-ndjson-join --left 'd.localidade' \
+ndjson-join --left 'd.Localidade' \
   geo2-br_municipios.ndjson \
   dado2-ensino-quilombo-comchave.ndjson \
   | ndjson-map \
